f344bced3590b44f8da02943478e36e9cfa2ab56
Dat Nguyen <tdat.nguyen@zscaler.com>
2024-03-21 20:49:02 -0700

    fixed weird bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+        /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/tipme",
     // Use the CDN in production and localhost for development.
     assetPrefix: process.env.NODE_ENV === 'prod' ? 'https://dat.works' : undefined,
 };
