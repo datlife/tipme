a000d32012dfc797054ec5c85e8a487879351333
Dat Nguyen <tdat.nguyen@zscaler.com>
2024-03-21 20:22:36 -0700

    Fixed assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? 'https://dat.works' : undefined,
 };
 
 export default nextConfig;
