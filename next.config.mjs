15b96d5a5cd92326134fd3c8df8494440ee42873
Dat Nguyen <tdat.nguyen@zscaler.com>
2024-03-21 20:25:48 -0700

    Add podinfo GitRepository

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://dat.works' : undefined,
+    assetPrefix: process.env.NODE_ENV === 'prod' ? 'https://dat.works' : undefined,
 };
 
 export default nextConfig;
